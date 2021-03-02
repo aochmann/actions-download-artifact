4b42ea4554ac384ebdb9eec6e727677a95e16aeb
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-03-02 10:29:44 +0100

    feat: added download artifact

---
+++ main.js
@@ -0,0 +1,94 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const AdmZip = require('adm-zip')
+const filesize = require('filesize')
+const moment = require('moment')
+const pathname = require('path')
+const fs = require("fs")
+
+async function main() {
+  try {
+    // required
+    const token = core.getInput("github_token", { required: true });
+    const [owner, repo] = core.getInput("repo", { required: true }).split("/");
+    const path = core.getInput("path", { required: true })
+
+    // optional
+    const artifactName = core.getInput("name", { required: false });
+    const latest = core.getInput("latest", { required: false });
+
+    const client = github.getOctokit(token);
+
+    console.log('input', path, artifactName, latest);
+
+    console.log("==> Repo:", owner + "/" + repo);
+
+    const artifactsEndpoint = "GET /repos/:owner/:repo/actions/artifacts";
+    const artifactsEndpointParams = {
+      owner: owner,
+      repo: repo,
+      per_page = 100
+    };
+
+    let artifacts = [];
+
+    for await (const artifactResponse of client.paginate
+      .iterator(artifactsEndpoint, artifactsEndpointParams)) {
+      console.log('artifacts data', artifactResponse, artifactResponse.data);
+
+      artifacts.push(artifactResponse.data
+        .filter(artifact => !artifact.expired)
+        .filter(artifact => artifactName && !latest ? artifact.name === artifactName : true)
+      );
+    }
+
+    if (latest && artifacts && artifacts.length) {
+      var latestArtifact = artifacts.reduce((prev, cur, index) => {
+        var prevDate = new moment(prev.updated_at);
+        var curDate = new moment(cur.updated_at);
+
+        return curDate > prevDate && index ? cur : prev;
+      });
+
+      if (latestArtifact) {
+        artifacts = [ latestArtifact ];
+      }
+    }
+
+    if (artifacts && artifacts.length) {
+      for (const artifact of artifacts) {
+        console.log("==> Artifact:", artifact.id);
+
+        const size = filesize(artifact.size_in_bytes, { base: 10 });
+
+        console.log("==> Downloading:", artifact.name + ".zip", `(${size})`);
+
+        const zip = await client.actions.downloadArtifact({
+          owner: owner,
+          repo: repo,
+          artifact_id: artifact.id,
+          archive_format: "zip",
+        });
+
+        const dir = artifactName ? path : pathname.join(path, artifact.name);
+
+        fs.mkdirSync(dir, { recursive: true });
+
+        const adm = new AdmZip(Buffer.from(zip.data));
+
+        adm.getEntries().forEach((entry) => {
+          const action = entry.isDirectory ? "creating" : "inflating";
+          const filepath = pathname.join(dir, entry.entryName);
+
+          console.log(`  ${action}: ${filepath}`)
+        });
+
+        adm.extractAllTo(dir, true);
+      }
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main()
